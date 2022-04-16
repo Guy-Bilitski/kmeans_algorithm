5fc652decac0d7a86a5a557a13c4b65caf782515
guyb1 <guyb181@gmail.com>
2022-04-16 20:43:09 +0300

    left to do: write to file, free memory, test

--- c/ex1.c
+++ c/ex1.c
@@ -10,44 +10,26 @@ int get_dimension(char *input_file); // returns the dimention d of all vectors
 void add_two_vectors(float *vec1, float *vec2); // adding vec2 to vec1
 
 
-
-
-void add_two_vectors(float *vec1, float *vec2) {
-    for (int i=0; i < sizeof(vec1); i++) {
-        vec1[i] += vec2[i];
-    }
-}
-
-
 int main(int argc, char **argv)
 {
     char *output_filename, *input_filename;
-    int k, maxiter, dim;
+    int k, maxiter = -1, dim;
     double **centroids;
+    double **new_centroids;
 
-    if (argc == 4){
+    if (argc == 4 || argc == 5) {
         k = atoi(argv[1]);
-        input_filename = argv[2];
-        output_filename = argv[3];
+        input_filename = argv[argc-2];
+        output_filename = argv[argc-1];
+        maxiter = argc==5 ? atoi(argv[2]) : INT_MAX;
+        if (maxiter <= 0 || k <= 0) {
+            printf("Invalid Input! 0");
+            return 1;
+        }
     }
 
-    else if (argc == 5) {
-        k = atoi(argv[1]);
-        maxiter = atoi(argv[2]);
-        input_filename = argv[3];
-        output_filename = argv[4];
-    }
     else {
         printf("Invalid Input! 1");
-        return 1;
-    }
-
-    /* Args validations:
-        1. k is integer bigger than 0
-        2. max itter is bigger than 0, if exists
-    */
-    if ( k <= 0 || (argc==5 && maxiter<=0) ){
-        printf("Invalid Input! 2");
         return 1;
     }
 
@@ -59,9 +41,79 @@ int main(int argc, char **argv)
 
     centroids = initialize_centroids(k, dim, input_filename);
 
-    
+    for (int i=0; i < maxiter; i++) {
+        new_centroids = kmeans_iteration(k, dim, input_filename, centroids);
+        if (max_distance_between_centroids(k, dim, centroids, new_centroids) < 0.001) {
+            break;
+        }
+    }
+
+    centroids = new_centroids;
+
+    for (int i = 0; i < k; i++) {
+        
+        for (int j = 0; j < dim; j++) {
+            printf("%lf,", &datapoints[i][j]);
+        }
+
+        datapoints[0][dim] = 1;
+
+    }
+
+
 
     return 0;
+}
+
+
+double max_distance_between_centroids(int k, int dim, double **old_centroids, double **new_centroids) {
+    double max_value = DBL_MAX;
+    double current_value;
+
+    for (int i=0; i < k; i++) {
+        current_value = 0;
+        for (int j=0; k < dim; j++) {
+            current_value = pow((old_centroids[i][j] / old_centroids[i][dim]) - (new_centroids[i][j] / new_centroids[i][dim]), 2);
+            if (current_value > max_value) {
+                max_value = current_value;
+            }
+        }
+    } 
+
+    return max_value;
+}
+
+
+double **kmeans_iteration(int k, int dim, char *input_file, double **centroids) {
+    FILE *ifp;
+    double **new_centroids;
+    int closet_centroid_index;
+    new_centroids = calloc(k, sizeof(double *));
+    for (int i=0; i < k; i++) {
+        new_centroids[i] = calloc(dim + 1, sizeof(double));
+    }
+    double *vector;
+    vector = calloc(dim, sizeof(double));
+
+    ifp = fopen(input_file, "r");
+    if (ifp == NULL) {
+        printf("An Error Has Occurred");
+        return NULL;
+    }
+
+    while (!feof(ifp)) {
+        for (int j = 0; j < dim; j++) {
+            fscanf(ifp, "%lf,", &vector[j]);
+        }
+
+        closet_centroid_index = find_closest_centroid(k, dim, vector, centroids);
+
+        for (int j = 0; j < dim; j++) {
+            new_centroids[closet_centroid_index][j] += vector[j];
+        }
+
+        new_centroids[closet_centroid_index][dim] ++;
+    }
 }
 
 int find_closest_centroid(int k, int dim, double *vector, double **centroids) {
@@ -86,11 +138,10 @@ int find_closest_centroid(int k, int dim, double *vector, double **centroids) {
 
 double **initialize_centroids(int k, int dim, char *input_file) {
     FILE *ifp;
-    float num;
     double **datapoints;
-    datapoints = calloc(k*(dim + 1), sizeof(double));
+    datapoints = calloc(k, sizeof(double *));
     for (int i=0; i < k; i++) {
-        datapoints[i] = calloc(dim, sizeof(double));
+        datapoints[i] = calloc(dim + 1, sizeof(double));
     }
     ifp = fopen(input_file, "r");
     if (ifp == NULL) {
@@ -102,21 +153,14 @@ double **initialize_centroids(int k, int dim, char *input_file) {
         
         for (int j = 0; j < dim; j++) {
             fscanf(ifp, "%lf,", &datapoints[i][j]);
-            // printf("%lf", datapoints[i][j]);
-            // printf("\t");
         }
         datapoints[0][dim] = 1;
 
     }
 
-    // Now we need to read k first rows and insert all values as centroids to the given array
-
-
-
     fclose(ifp);
+    return datapoints;
 }
-
-
 
 int get_dimension(char *input_file) {
     FILE *ifp = NULL;
